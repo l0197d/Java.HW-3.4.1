03dc0d306dd7d3d583290bdcea5903967835ceaa
l0197d <titus04@yandex.ru>
2021-03-11 21:29:51 +0300

    После доработки

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -3,32 +3,53 @@ package ru.netology.manager;
 import ru.netology.domain.Movie;
 
 public class AfishaManager {
-  private Movie[] poster = new Movie[0];
-  private int filmQuantity = 10;
+    private Movie[] poster = new Movie[0];
+    private int filmQuantity = 10;
 
-  public AfishaManager() {
+    public AfishaManager() {
 
-  }
-
-  //Добавить новый фильм
-  public void add(Movie newMovie) {
-    int length = poster.length + 1;
-    Movie[] newPoster = new Movie[length];
-    for (int i = 0; i < poster.length; i++) {
-      newPoster[i] = poster[i];
     }
-    newPoster[newPoster.length - 1] = newMovie;
-    poster = newPoster;
-  }
 
-  // Показать последний фильм
-  public Movie[] getLast() {
-    int quantity = Math.min(filmQuantity, poster.length);
-    Movie[] result = new Movie[quantity];
-    for (int i = 0; i < quantity; i++) {
-      int index = poster.length - 1 - i;
-      result[i] = poster[index];
+    public AfishaManager(int filmQuantity) {
+        this.filmQuantity = filmQuantity;
     }
-    return result;
-  }
+
+
+    //Метод для добавления фильмов
+    public void add(Movie newMovie) {
+        int length = poster.length + 1;
+        Movie[] newPoster = new Movie[length];
+        for (int i = 0; i < poster.length; i++) {
+            newPoster[i] = poster[i];
+        }
+        newPoster[newPoster.length - 1] = newMovie;
+        poster = newPoster;
+    }
+
+    //Метод получения всего списка элементов
+    public Movie[] getAll() {
+        int quantity = Math.min(filmQuantity, poster.length);
+        Movie[] result = new Movie[quantity];
+        for (int i = 0; i < quantity; i++) {
+            int index = poster.length - 1 - i;
+            result[i] = poster[index];
+        }
+        return result;
+    }
+
+    //Метод, который выводит 10 фильмов
+    public Movie[] getMovies() {
+        int feed = this.filmQuantity;
+        if (feed > poster.length)
+            feed = poster.length;
+        Movie[] result = new Movie[feed];
+        for (int i = 0; i < result.length; i++) {
+            int index = poster.length - i - 1;
+            result[i] = poster[index];
+        }
+        return result;
+
+    }
+
+
 }
